018b1de148a7ed2e501c375b1cdcc77f17a01521
jihun-heo <karais89@gmail.com>
2022-10-03 10:48:25 +0900

    구글 통계 추가

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -50,6 +50,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'UA-59335977-2',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
