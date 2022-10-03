13a6ec93cd390406de58de7629960db3a47e93a5
jihun-heo <karais89@gmail.com>
2022-10-03 16:10:14 +0900

    지원 언어 csharp 추가

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -130,6 +130,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['csharp'],
       },
       colorMode: {
         respectPrefersColorScheme: true,
