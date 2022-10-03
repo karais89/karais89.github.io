5088ab6049bc7c2a15403a38f55e6b682ce5e38f
jihun-heo <karais89@gmail.com>
2022-10-03 20:37:01 +0900

    blog path 수정

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -65,6 +65,7 @@ const config = {
         language: ["ko", "en"],
         highlightSearchTermsOnTargetPage: true,
         explicitSearchResultPath: true,
+        blogRouteBasePath: "/",
       },
     ],
   ],
