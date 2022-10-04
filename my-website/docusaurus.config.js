47247dbe8b75741b75617f518ae06e0428e67666
jihun-heo <karais89@gmail.com>
2022-10-05 00:02:30 +0900

    카피라이트 변경

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -137,7 +137,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Lonpeach, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
